811d373c89af417d148af83bf258d45e80dbded7
carlesDeveloper <carles_blaugrana@hotmail.com>
2024-04-11 19:10:14 +0200

    styles improving assigning 92vh every section

--- frontend_personal_page/src/About.tsx
+++ frontend_personal_page/src/About.tsx
@@ -5,18 +5,21 @@ import ContactButtons from './ContactButtons';
 function About() {
     return(
         <>
-            <div className='title__page'>
-                <h2>About me</h2>
+            <div className='about__content'>
+                <div className='about__title'>
+                    <h2>About me</h2>
+                </div>
+                <div className="about__text">
+                    <p>Web Developer with 5 years of experience, responsible for creating software solutions. </p>
+                    <p>Possesses advanced proficiency in JavaScript, React JS, Docker, R, Python, 
+                        and expertise in the Linux operating system. Focused on problem-solving, meticulous 
+                        attention to detail, effective time management, and accustomed to collaborative teamwork. </p>
+                    <p>Holds a degree in Business Statistics and a Master's in Web analytics, 
+                        combining my two major passions—software development and data.</p>
+                </div> 
+                <ContactButtons />
             </div>
-            <div className='content__page'>
-                <p>Web Developer with 5 years of experience, responsible for creating software solutions. </p>
-                <p>Possesses advanced proficiency in JavaScript, React JS, Docker, R, Python, 
-                    and expertise in the Linux operating system. Focused on problem-solving, meticulous 
-                    attention to detail, effective time management, and accustomed to collaborative teamwork. </p>
-                <p>Holds a degree in Business Statistics and a Master's in Web analytics, 
-                    combining my two major passions—software development and data.</p>
-            </div>
-            <ContactButtons />
+            
         </>
     )
 }
