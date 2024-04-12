c8693635648ec343b26340a07be488bc6bfa6e79
carlesDeveloper <carles_blaugrana@hotmail.com>
2024-04-12 22:18:00 +0200

    clifton strength animation

--- frontend_personal_page/src/Strength.tsx
+++ frontend_personal_page/src/Strength.tsx
@@ -8,12 +8,12 @@ function Strength() {
                 <p className='title__section'>Clifton Strengths</p>
             
                 <div className="strength__skills">
-                    <div className="skill strategy_thinking">Analytical</div>
-                    <div className="skill strategy_thinking">Learner</div>
-                    <div className="skill influencing">Communication</div>
-                    <div className="skill executing">Achiever</div>
-                    <div className="skill relationship_building">Empathy</div>
-                    <div className="skill relationship_building">Positivity</div>
+                    <div className="skill strategy_thinking skill1">Analytical</div>
+                    <div className="skill strategy_thinking skill2">Learner</div>
+                    <div className="skill influencing skill3">Communication</div>
+                    <div className="skill executing skill4">Achiever</div>
+                    <div className="skill relationship_building skill5">Empathy</div>
+                    <div className="skill relationship_building skill6">Positivity</div>
                 </div>
             </div>
         </>
